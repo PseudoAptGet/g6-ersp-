d116fcd152a99fc72796b0ae0de2a8685f7336f9
PseudoAptGet <ananabrar02@gmail.com>
2022-02-17 10:25:44 -0500

    setup

---
+++ G6-ERSP-/src/App.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        try (BufferedReader br = new BufferedReader(new FileReader("data/StudentProblemHistory_October18(1).csv"))) {
+            String line;
+            line = br.readLine();
+            line = br.readLine();
+            ArrayList<String> record = new ArrayList<String>(Arrays.asList(line.split(",")));
+
+
+            System.out.println(record.get(4));
+            // while ((line = br.readLine()) != null) {
+            //     String[] values = line.split("'");
+            //     records.add(Arrays.asList(values));
+            // }
+        }
+        System.out.println(App.getNumStudents());
+
+        // data/StudentProblemHistory_October18(1).csv
+
+
+
+    }
+
+    public static int getNumStudents(){
+
+        HashSet<Integer> x = new HashSet<Integer>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("data/StudentProblemHistory_October18(1).csv"))) {
+            String line;
+            line = br.readLine();
+
+            while((line = br.readLine()) != null){
+     
+                ArrayList<String> record = new ArrayList<String>(Arrays.asList(line.split(",")));
+
+
+                System.out.println(record.get(4));
+                x.add(Integer.parseInt(record.get(4)));
+                
+            }
+            
+        }
+        
+        catch (FileNotFoundException ex){
+
+        } 
+        catch (IOException ex){
+
+        }
+        return x.size();
+
+    }
+}
--- regAnalysis/importingData.py
+++ regAnalysis/importingData.py
@@ -1,1 +1,12 @@
+# great resource to help setup: https://realpython.com/python-csv/
+# READ THE DOCUMENTATION FOR THIS: https://www.protechtraining.com/blog/post/python-for-beginners-reading-manipulating-csv-files-737#looping-through-rows
+# GIT BASICS: https://www.atlassian.com/git
+
+
+#
+import csv 
+import json
+import os
+
+print(os.listdir())
 
