1cf75491565057d3bb9f7b102aaab12b83e41f1d
PseudoAptGet <70147090+PseudoAptGet@users.noreply.github.com>
2022-04-19 09:01:53 -0400

    Update teacherAnalysis.java
    better comments to teacherAnalysis.java

--- teacherAnalysis.java
+++ teacherAnalysis.java
@@ -6,34 +6,27 @@ public class studentClustering {
 
     public static void main(String[] args){
         System.out.println(studentClustering.getNumTeachers());
-
-    
-
-
-
-
     }
     
+    //calculates the number of teachers who have done at least 10 actions. 
     public static int getNumTeachers(){
-
-        HashMap<Integer, Integer> x = new HashMap<Integer, Integer>(); //hashset 
+        HashMap<Integer, Integer> x = new HashMap<Integer, Integer>(); // x has a key value map of teacher id : number of actions
         int numTeachers = 0;  // tracker of numStudents
 
-        try (BufferedReader br = new BufferedReader(new FileReader("data/TeacherActivityOctober18.csv"))) { // FELLAS, CHANGE YOUR FILENAME, AND ADD CORRECT PATH. 
-            String line;
-            line = br.readLine(); //SKIP THE FIRST LINE BC ITS THE HEADERS/USELESS
-            while((line = br.readLine()) != null){ //KEEP ON READING LINES
+        try (BufferedReader br = new BufferedReader(new FileReader("data/TeacherActivityOctober18.csv"))) { //Change as necessary to correct filepath
+            line = br.readLine(); //skips the first line since it's useless
+            while((line = br.readLine()) != null){ 
                 
-                ArrayList<String> record = new ArrayList<String>(Arrays.asList(line.split(","))); //SPLIT ON COMMAS. GET ARRAYLIST OF ELEMENTS IN A ROW
-                String y = record.get(1);   //GET PARTICULAR ELEMENT AT ?,IMPORTANT,.....
+                ArrayList<String> record = new ArrayList<String>(Arrays.asList(line.split(","))); 
+                String y = record.get(1);   
                 
-                if(x.containsKey(Integer.parseInt(y))){ //HASHMAP. USELESS, BUT CHECK JAVADOC
+                if(x.containsKey(Integer.parseInt(y))){ //this entry already exists. Increment x
                     int newVal = x.get(Integer.parseInt(y));
                     int next = newVal+1;
                     x.replace(Integer.parseInt(y), next);
                 }
                 else{
-                    x.put(Integer.parseInt(y), 1);
+                    x.put(Integer.parseInt(y), 1); //entry doesn't exist. Create it in x
                 }
                 
                 
@@ -45,17 +38,18 @@ public class studentClustering {
                 }
             }
 
-            StringBuilder sb = new StringBuilder(); //FOR DEBUGGING
-            for (Map.Entry<Integer, Integer> entry : x.entrySet()) {
-                sb.append(entry.getKey());
-                sb.append('=');
-                sb.append(entry.getValue());
-                sb.append('&');
-            }
-            sb.deleteCharAt(sb.length() - 1);
-            String result = sb.toString();
+            // debugging purposes. 
+//             StringBuilder sb = new StringBuilder(); //FOR DEBUGGING PURPOSES
+//             for (Map.Entry<Integer, Integer> entry : x.entrySet()) {
+//                 sb.append(entry.getKey());
+//                 sb.append('=');
+//                 sb.append(entry.getValue());
+//                 sb.append('&');
+//             }
+//             sb.deleteCharAt(sb.length() - 1);
+//             String result = sb.toString();
 
-            System.out.println(result);
+//             System.out.println(result);
             
         }
         
